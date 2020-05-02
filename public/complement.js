c895edf0afbc6821919049080aae5293d022f638
JasperS <jasperspringael@hotmail.com>
2020-05-02 03:22:56 +0200

    updated version using express

---
+++ public/complement.js
@@ -0,0 +1,13 @@
+document.querySelector('.request-complement')
+.addEventListener('click',function() {
+    fetch ('/complement')
+    .then(function (res) {
+        return res.json ();
+    })
+    .then(function(data) {
+        document.querySelector('.complement').innerText = data.complement;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+});
